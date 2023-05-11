a261812d01b035fbc6e76b92cb372267da12f0d0
fruzelee <fazle.rabbi.cse@gmail.com>
2023-05-11 12:08:52 +0600

    feat: add LazyVerticalGrid with hundreds of items

--- app/src/main/java/com/example/lazyverticalgridcompose/MainActivity.kt
+++ app/src/main/java/com/example/lazyverticalgridcompose/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.lazyverticalgridcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lazyverticalgridcompose.ui.theme.LazyVerticalGridComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,13 +25,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LazyVerticalGridComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(4),
+                    content = {
+                        items(100) { i ->
+                            Box(
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .aspectRatio(1f)
+                                    .clip(RoundedCornerShape(5.dp))
+                                    .background(Color.Green),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Text(text = "Item $i")
+                            }
+                        }
+                    }
+                )
             }
         }
     }
